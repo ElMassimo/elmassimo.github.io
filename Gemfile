0ada7e7eb7abd78b6e69eca96bfd2f972f832e6d
Máximo Mussini <mmussini@cipherhealth.com>
2015-09-11 23:09:53 -0300

    Less :guitar: Add helper scripts :neckbeard:

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+
+gem 'github-pages'
+gem 'mr_poole'
+gem 'octopress-autoprefixer'
+gem 'rake'
+
+group :development do
+  gem 'pry-byebug'
+end
---
+++ assets/js/shortcuts.js
@@ -0,0 +1,31 @@
+LEFT = 37; UP = 38; RIGHT = 39; DOWN = 40;
+
+document.onkeydown = function(e) {
+  switch (e.keyCode) {
+    case LEFT: goToNextPost(); break;
+    case UP: if(scrolledToTop()) { goToNextPost() } break;
+    case RIGHT: goToPreviousPost(); break;
+    case DOWN: if(scrolledToBottom()) { goToPreviousPost() } break;
+  }
+};
+
+function clickById(id) {
+  var el = document.getElementById(id);
+  if (el) el.click()
+}
+
+function goToPreviousPost() {
+  clickById('previous-post');
+}
+
+function goToNextPost() {
+  clickById('next-post');
+}
+
+function scrolledToTop() {
+  return window.scrollY == 0;
+}
+
+function scrolledToBottom() {
+  return (window.innerHeight + window.scrollY) >= document.body.offsetHeight;
+}
