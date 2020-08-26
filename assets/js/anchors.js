bcccbdb09d9f7430bcd586726b6f37a8f43534e6
Maximo Mussini <maximomussini@gmail.com>
2020-08-26 12:06:49 -0300

    Accessibility Improvements

--- assets/js/anchors.js
+++ assets/js/anchors.js
@@ -1,14 +1,16 @@
-function appendLinkToHeading(heading) {
-  if(heading.id) {
+function appendLinkToHeading (heading) {
+  if (heading.id) {
     var anchor = document.createElement('a');
-    anchor.setAttribute('href', '#' + heading.id);
-    anchor.setAttribute('class', 'anchor');
+    anchor.setAttribute('class', 'anchor')
+    anchor.setAttribute('href', '#' + heading.id)
+    anchor.setAttribute('aria-hidden', 'true')
+    anchor.setAttribute('tabindex', '-1')
     heading.insertBefore(anchor, heading.firstChild)
   }
 }
 
 for (var i = 1; i <= 6; i++) {
-  headings = document.querySelectorAll('body > .content h' + i);
+  headings = document.querySelectorAll('body > .content h' + i)
 
   for (var j = 0; j < headings.length; j++)
     appendLinkToHeading(headings[j])
